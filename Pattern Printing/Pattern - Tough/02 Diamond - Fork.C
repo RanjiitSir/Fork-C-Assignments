864993309a0326613fc12bd3424aeb18480b10de
Ranjiit Sir - FORK INFOSYSTEMS <ranjiitsir@gmail.com>
2026-03-30 13:44:40 +0530

    Add files via upload

---
+++ Pattern Printing/Pattern - Tough/02 Diamond - Fork.C
@@ -0,0 +1,41 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int n, i, j;
+
+    printf("\n Enter number of rows (Full diamond): ");
+    scanf("%d", &n);    // 11
+
+    printf("\n Pattern is => \n\n");
+
+    // Upper half
+    for(i = 1; i <= n/2 + 1  ; i++)
+    {
+        for(j = 1; j <= (n/2 + 1) - i; j++)
+        {
+            printf("   ");
+        }
+        for(j = 1; j <= (2 * i) - 1; j++)
+        {
+                printf(" * ");
+        }
+        printf("\n");
+    }
+
+     // Lower half
+    for(i = 1; i <= n/2  ; i++)
+    {
+        for(j = 1; j <= i; j++)
+        {
+            printf("   ");
+        }
+        for(j = 1; j <= n - (2 * i); j++)
+        {
+                printf(" * ");
+        }
+        printf("\n");
+    }
+    return 0;
+}
